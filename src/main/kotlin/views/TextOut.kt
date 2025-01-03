158dd05db5002aca0f9cbc21bfc041a7b9f680c9
Paul Fischer <paultsemingfischer@gmail.com>
2025-01-03 17:55:45 -0500

    Adding TextOut code

--- src/main/kotlin/views/TextOut.kt
+++ src/main/kotlin/views/TextOut.kt
@@ -4,16 +4,39 @@ import models.GameState
 
 class TextOut : ViewOutput {
     fun push(gameState: GameState) {
-
+        println("\n".repeat(20))
+        println(generateGameInfo(gameState))
+        println(generateBoardFrame(gameState))
+        generatePlayerInfo(gameState).forEach { println(it) }
     }
 
-    fun generateFrame(gameState: GameState) {
+    fun generateGameInfo(gameState: GameState): String{
+        val gameInfo = StringBuilder()
+        gameInfo.append("Turn: ${gameState.turnNum} Pass Streak: ${gameState.passStreak}")
+        return gameInfo.toString()
+    }
+
+    fun generateBoardFrame(gameState: GameState): String {
         val screen: StringBuilder = StringBuilder()
         val screenLength = 100
-
-        screen.append("\n".repeat(20))
         screen.append("-".repeat(screenLength))
         screen.append(centerString("SCRABBLE", screenLength))
+        screen.append("*" + "-".repeat(gameState.board.size()) + "*")
+        for(row in gameState.board.board) {
+            print(centerString("|" + row.joinToString("") + "|", screenLength))
+        }
+        screen.append("*" + "-".repeat(gameState.board.size()) + "*")
+        return screen.toString()
+    }
+
+    fun generatePlayerInfo(gameState: GameState): List<String>{
+        val players = ArrayList<String>()
+        for(player in gameState.players){
+            val playerString = StringBuilder()
+            playerString.append("Player: ${player.name} Score: ${player.score} Hand: ${player.hand.pieces.joinToString("")}")
+            players.add(playerString.toString())
+        }
+        return players
     }
 
     fun centerString(text: String, length: Int, character: Char = ' '): String {

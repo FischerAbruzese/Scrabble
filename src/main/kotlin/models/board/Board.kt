3990c0b92cad51cdc70dc6e722a8f9b6947fb4ff
Paul Fischer <paultsemingfischer@gmail.com>
2025-01-03 17:45:47 -0500

    Board improvements

--- src/main/kotlin/models/board/Board.kt
+++ src/main/kotlin/models/board/Board.kt
@@ -1,12 +1,21 @@
 package models.board
 
-class Board {
+class Board(val board: Array<Array<Square>>) {
     //todo: Add multipliers
-    val board = Array(15) { Array(15) { Square(Multiplier.NONE) } }
+    constructor() : this(Array(15) { Array(15) { Square(Multiplier.NONE) } })
+
+    fun classInv(): Boolean{
+        return board.isNotEmpty() && board.size == board[0].size
+    }
 
     fun get(coord: Coord) = board[coord.x][coord.y]
 
     fun set(coord: Coord, square: Square) {
         board[coord.x][coord.y] = square
     }
+
+    /**
+     * Width and height of the board
+     */
+    fun size() = board.size
 }

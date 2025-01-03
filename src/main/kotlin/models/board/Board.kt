ee225b77378593584f664fc917d5a47504e46ada
Skylar <luke@abruzese.com>
2025-01-03 18:42:49 -0500

    Changes by Paul F

--- src/main/kotlin/models/board/Board.kt
+++ src/main/kotlin/models/board/Board.kt
@@ -4,7 +4,7 @@ class Board(val board: Array<Array<Square>>) {
     //todo: Add multipliers
     constructor() : this(Array(15) { Array(15) { Square(Multiplier.NONE) } })
 
-    fun classInv(): Boolean{
+    fun classInv(): Boolean {
         return board.isNotEmpty() && board.size == board[0].size
     }
 

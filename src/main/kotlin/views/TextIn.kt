8229b976e63d24800cfa282b34f88677869416a5
Paul Fischer <paultsemingfischer@gmail.com>
2025-01-03 21:07:49 -0500

    Hand removal methods

--- src/main/kotlin/views/TextIn.kt
+++ src/main/kotlin/views/TextIn.kt
@@ -1,4 +1,121 @@
 package views
 
-class TextIn : ViewInput {
+
+import controllers.players.PlayerController
+import models.turn.Direction
+import models.GameState
+import models.Player
+import models.board.Coord
+import models.tiles.Piece
+import models.turn.Exchange
+import models.turn.Move
+import models.turn.Pass
+import models.turn.Turn
+import kotlin.system.exitProcess
+
+class TextIn : ViewInput, PlayerController {
+
+    companion object {
+        const val WHITE_BOLD = "\u001B[1;37m"
+        const val RESET = "\u001B[0m"
+    }
+
+    override fun getTurn(gameState: GameState, player: Player): Turn {
+        return when (queryMoveType(player.name)) {
+            "M" -> queryMove(gameState, player)
+            "E" -> queryExchange(gameState, player)
+            "P" -> return Pass()
+            else -> exitProcess(0)
+        }
+    }
+
+    private fun queryMoveType(playerName: String): String {
+        while (true) {
+            println("${playerName}, what would you like to do? (M)ove, (E)xchange, (P)ass, (Q)uit?")
+            var input = readLine()
+            input = input?.toUpperCase()?.first().toString()
+            if (input == "M" || input == "E" || input == "P" || input == "Q") return input
+            else println("Invalid input: $input")
+        }
+    }
+
+    private fun queryExchange(gameState: GameState, player: Player): Turn {
+        while (true) {
+            println("Which pieces would you like to exchange? (format ex: AA_B)")
+            var input = readLine()
+            input = input?.trim()?.uppercase()
+            if(input == null){
+                println("Invalid input: $input")
+                continue
+            }
+            if(input!!.length > gameState.bag.size()){
+                println("Not enough pieces left in the bag")
+                continue
+            }
+            val removedPieces = player.hand.removePieces(input!!.toCharArray().toList())
+            if(removedPieces == null)
+                println("You do not have the required pieces to exchange.")
+            else return Exchange(removedPieces)
+        }
+    }
+
+    private fun queryMove(gameState: GameState, player: Player): Turn {
+        while(true) {
+            //Tiles
+            var tiles: List<Piece>? = null
+            while (tiles == null) {
+                println("Which pieces would you like to place(skip pieces already on the board but keep the same order as the word)? (format ex: SCRBLE if forming the word SCRABBLE and AB is already on the board)")
+                var input = readLine()
+                input = input?.trim()?.uppercase()
+                if (input == null) {
+                    println("Invalid input: $input")
+                    continue
+                }
+                val removedPieces = player.hand.removePieces(input!!.toCharArray().toList())
+                if (removedPieces == null) {
+                    println("You do not have the required pieces to play that.")
+                    continue
+                } else tiles = removedPieces
+            }
+
+            //Coordinates
+            var coord: Coord? = null
+            while (coord == null) {
+                println("Where would you like to place those pieces? (format: row,col -- ex: 0,0)")
+                var input = readLine()
+                input = input?.filter { it.isDigit() || it == ',' }
+                if (input == null) {
+                    println("Invalid input: $input")
+                    continue
+                }
+                val coords: List<Int>
+                try {
+                    coords = input.split(",").map { it.toInt() }
+                } catch (e: NumberFormatException) {
+                    println("Invalid input: $input")
+                    continue
+                }
+                if (coords.size != 2) {
+                    println("Invalid input: $input")
+                    continue
+                }
+                coord = Coord(coords[0], coords[1])
+            }
+
+            //Direction
+            var direction: Direction? = null
+            if(tiles.size == 1) direction = Direction.NONE
+            while (direction == null){
+                println("What direction would you like to place the pieces? (D)own, (A)cross")
+                var input = readLine()
+                input = input?.toUpperCase()?.first().toString()
+                if(input == "D") direction = Direction.DOWN
+                else if(input == "A") direction = Direction.ACROSS
+                else println("Invalid input: $input")
+            }
+
+            return Move(coord, direction, tiles)
+        }
+
+    }
 }

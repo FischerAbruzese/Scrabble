27062c02d3247b0649559baa65f4b4aa8dcd2bf6
Paul Fischer <paultsemingfischer@gmail.com>
2025-01-05 01:14:46 -0500

    Removing proxy

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
-        proxy: {
-            '/game-state': {
-                target: 'ws://localhost:8080',
-                ws: true,
-            }
-        }
     }
 })

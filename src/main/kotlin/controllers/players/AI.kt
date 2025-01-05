874d847d1a87b1f27c59f6db50633efca6b26dea
Paul Fischer <paultsemingfischer@gmail.com>
2025-01-05 15:18:19 -0500

    Continuing work on AI

--- src/main/kotlin/controllers/players/AI.kt
+++ src/main/kotlin/controllers/players/AI.kt
@@ -7,9 +7,24 @@ import models.turn.Move
 import models.turn.Turn
 
 class AI : PlayerController {
+    //AI Parameters
+    /**
+     * Minimum score of a move to be considered. Must be greater than 0
+     */
+    private val moveScoreCutoff = 3
+
+
+
     override fun getTurn(gameState: GameState, player: Player): Turn {
-        var bestMoveScore = 0
-        var bestMove: Move? = null
+        val (bestMove: Move?, bestMoveScore: Int) = maxOf(
+            bestMove(gameState, player, Direction.ACROSS),
+            bestMove(gameState, player, Direction.DOWN),
+            comparator = compareBy{it.second}
+        )
+
+        if(bestMoveScore > moveScoreCutoff && bestMove != null) return bestMove
+
+
 
     }
 
@@ -18,7 +33,8 @@ class AI : PlayerController {
      *
      * @return the best move and its score
      */
-    private fun bestMove(gameState: GameState, player: Player, direction: Direction): (Move, Int)? {
+    private fun bestMove(gameState: GameState, player: Player, direction: Direction): Pair<Move?, Int> {
+        var bestMoveScore = -1
         for(emptySquare in gameState.board.filter { !it.hasPiece() }){
 
         }

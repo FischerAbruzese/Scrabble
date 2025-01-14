f1d7519ecba0a25be0898ec79a527c4a89190b91
Skylar <luke@abruzese.com>
2025-01-14 17:55:40 -0500

    finished basic framework for matrix

--- src/main/kotlin/util/Matrix.kt
+++ src/main/kotlin/util/Matrix.kt
@@ -1,11 +1,15 @@
 package util
 
-class Matrix<T>(rowCount: Int, colCount: Int, init: (Int, Int) -> T) {
+class Matrix<T>(private val rowCount: Int, private val colCount: Int, init: (Int, Int) -> T) {
 
     @Suppress("UNCHECKED_CAST")
-    private val secretMatrix = Array(rowCount) { r ->
-        Array<Any?>(colCount) { c -> init(r,c) }
-    } as Array<Array<T>>
+    private val secretArray = Array<Any?>(rowCount * colCount) { i ->
+        val (row, col) = getRowCol(i)
+        init(row, col)
+    } as Array<T>
+    private fun getRowCol(index: Int) = index / colCount to index % colCount
+    private fun getIndex(row: Int, col: Int) = row*colCount + col
 
-
+    operator fun get(row: Int, col: Int) = secretArray[getIndex(row, col)]
+    operator fun set(row: Int, col: Int, value: T) { secretArray[getIndex(row, col)] = value }
 }
